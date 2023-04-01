74f96e5e53e4e1885d6cfd28b0395b2d20adaebf
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-04-01 15:11:49 +0330

    Add djangorestframework library

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings/base.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings/base.py
@@ -10,6 +10,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core',
 ]
 
@@ -82,3 +83,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_PAGINATION_CLASS': 'custom.pagination.GlobalLimitOffsetPagination',
+}
--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings/prod.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings/prod.py
@@ -6,3 +6,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 
 ALLOWED_HOSTS = []
+
+REST_FRAMEWORK.update(
+    {
+        'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
+    }
+)
