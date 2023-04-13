ba11b0a5427564b88b4a286d200ed762d79831a6
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-04-13 21:50:23 +0330

    Manage media and static files

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/urls.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/urls.py
@@ -1,6 +1,7 @@
-from django.conf import settings
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -22,5 +23,6 @@ if settings.DEBUG:
     urlpatterns.extend(
         [
             path("__debug__/", include("debug_toolbar.urls")),
+            *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
         ]
     )
