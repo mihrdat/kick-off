5c0d26712562b4bef7964820a7bac82f1ff37244
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-04-13 16:25:13 +0330

    Refactor in main urls.py

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/urls.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/urls.py
@@ -1,27 +1,26 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularSwaggerView,
     SpectacularRedocView,
+    SpectacularSwaggerView,
 )
 
-spectacular_urls = [
-    path("", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path(
-        "redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
-    ),
+swagger_urls = [
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
 urlpatterns = [
-    path("__debug__/", include("debug_toolbar.urls")),
+    path("", include(swagger_urls)),
     path("admin/", admin.site.urls),
-    path("schema/", include(spectacular_urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(
+        [
+            path("__debug__/", include("debug_toolbar.urls")),
+        ]
+    )
