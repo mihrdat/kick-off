21bdb330531d1dad30ac70a962a1f6c94692b0f7
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-04-17 10:03:15 +0330

    Add INTERNAL_IPS to debug_toolbar config

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/conf/debug_toolbar.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/conf/debug_toolbar.py
@@ -1,2 +1,6 @@
-# To fix django-debug-toolbar disappearing when running application with Docker.
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+# To fix django-debug-toolbar disappearing when running application using Docker.
 DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda request: True}
