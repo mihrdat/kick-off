7c9cced0e7a264f4acafb1d66cc37236725bb07a
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-04-27 11:35:53 +0330

    Manage urls in the config directory

--- {{ cookiecutter.project_slug }}/config/urls/main.py
+++ {{ cookiecutter.project_slug }}/config/urls/main.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include("config.swagger.urls")),
+    path("", include("config.urls.swagger")),
     path("admin/", admin.site.urls),
 ]
 
