23a68ab37d009f062f9b5e8058b9169a72e5d24c
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-04-29 10:06:35 +0330

    Fix a bug in cookiecutter.json

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/apps.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class {{ cookiecutter.project_name }}Config(AppConfig):
+class {{ cookiecutter.project_slug.title()|replace('_', '') }}Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "{{ cookiecutter.project_slug }}"
