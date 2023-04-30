b3ecc33d399939b750a19f32fc549e0316d5c9d6
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-04-30 22:03:58 +0330

    Configure email backend

--- {{ cookiecutter.project_slug }}/config/settings/dev.py
+++ {{ cookiecutter.project_slug }}/config/settings/dev.py
@@ -31,3 +31,9 @@ REST_FRAMEWORK = {
     **REST_FRAMEWORK,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "127.0.0.1"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = 25
