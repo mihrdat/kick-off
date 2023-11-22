d5f74e7c9d8a87d249be4391900a05103c2235e1
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-11-22 10:45:20 +0330

    Config email settings

--- {{ cookiecutter.project_slug }}/config/settings/dev.py
+++ {{ cookiecutter.project_slug }}/config/settings/dev.py
@@ -13,9 +13,3 @@ MIDDLEWARE += [
 
 # To fix django-debug-toolbar disappearing when running application using Docker.
 DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda request: True}
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp4dev"
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
-EMAIL_PORT = 25
