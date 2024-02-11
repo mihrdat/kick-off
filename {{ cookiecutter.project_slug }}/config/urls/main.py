4066d307b4eae86a04c67be29a2d85482ffaf46c
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-02-11 14:54:14 +0330

    Remove api url module

--- {{ cookiecutter.project_slug }}/config/urls/main.py
+++ {{ cookiecutter.project_slug }}/config/urls/main.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("swagger/", include("config.urls.swagger")),
-    path("api/v1/", include("config.urls.api")),
 ]
 
 if settings.DEBUG:
