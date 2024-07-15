68cc0a7370f15a174913fe39e988d366aff4f3ad
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-07-15 11:05:10 +0330

    Rename celery variable to app

--- {{ cookiecutter.project_slug }}/config/celery.py
+++ {{ cookiecutter.project_slug }}/config/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
-celery = Celery("{{ cookiecutter.project_slug }}")
-celery.config_from_object("django.conf:settings", namespace="CELERY")
-celery.autodiscover_tasks()
+app = Celery("{{ cookiecutter.project_slug }}")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
